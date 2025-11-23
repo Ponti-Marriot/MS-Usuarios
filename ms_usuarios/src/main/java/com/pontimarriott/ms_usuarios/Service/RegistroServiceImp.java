bb9d7cc4b6f81664fb9d7f739c063b1493dbd7b1
LV860 <plays867yer@gmail.com>
2025-11-22 22:53:19 -0500

    Arreglo de registro de usuario

--- ms_usuarios/src/main/java/com/pontimarriott/ms_usuarios/Service/RegistroServiceImp.java
+++ ms_usuarios/src/main/java/com/pontimarriott/ms_usuarios/Service/RegistroServiceImp.java
@@ -1,20 +1,29 @@
 package com.pontimarriott.ms_usuarios.Service;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import com.pontimarriott.ms_usuarios.Model.*;
+
+
+import com.pontimarriott.ms_usuarios.Model.Empleado;
+import com.pontimarriott.ms_usuarios.Model.EmpleadoNuevo;
 import com.pontimarriott.ms_usuarios.Repository.RepositoryRegistrarse;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.http.HttpMethod;
+
 
 
 @Service
@@ -36,25 +45,25 @@ public class RegistroServiceImp implements RegistroService{
     public String getAdminToken() {
         RestTemplate restTemplate = new RestTemplate();
 
+        String tokenUrl = "http://localhost:9000/realms/myrealm/protocol/openid-connect/token";
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
+        // Body igual al curl
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("grant_type", "password");
-        body.add("client_id", "admin-cli"); // o el cliente con rol admin
-        body.add("username", "admin");      // usuario admin del realm
-        body.add("password", "admin123");
+        body.add("grant_type", "client_credentials");
+        body.add("client_id", "admin-client");
+        body.add("client_secret", "4inrWXpnTE1dQoOEsT80gHNRpTpMhIlJ");
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
 
-        ResponseEntity<Map> response = restTemplate.postForEntity(
-                loginUrl,
-                request,
-                Map.class
-        );
+        ResponseEntity<Map> response =
+                restTemplate.postForEntity(tokenUrl, request, Map.class);
 
         return (String) response.getBody().get("access_token");
     }
+
 
 
     @Override
@@ -89,6 +98,9 @@ public class RegistroServiceImp implements RegistroService{
             String location = response.getHeaders().getFirst("Location");
             if (location != null) {
                 String userId = location.substring(location.lastIndexOf("/") + 1);
+
+                //assignEmpleadoRole(userId);
+
                 Empleado usuarioInsertar = new Empleado(userId, user.getNombre(),user.getCorreo());
                 Empleado usuario = registrarseRepository.save(usuarioInsertar);
                 messageEmpleadoService.sendMessageEmpleado(usuario);
@@ -96,6 +108,31 @@ public class RegistroServiceImp implements RegistroService{
             }
         }
         return null;
+    }
+    public void assignEmpleadoRole(String userId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://localhost:9000/admin/realms/myrealm/users/" 
+                    + userId 
+                    + "/role-mappings/clients/af907948-5cce-444f-b92b-43ccbaf153a9";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(getAdminToken());
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Rol que se va a asignar
+        Map<String, Object> role = new HashMap<>();
+        role.put("id", "c03e5b88-fb8f-432a-b35e-707b6e9a0b67");   // UUID del rol empleado
+        role.put("name", "empleado");                             // nombre del rol
+
+        HttpEntity<Object> request = new HttpEntity<>(List.of(role), headers);
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                request,
+                Void.class
+        );
     }
     
 }
